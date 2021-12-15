a5ec87c9bbaa2f9580e1968bd25eebb75e66d5be
Victor Silva de Franca <victor.silva.franca18@gmail.com>
2021-12-15 19:45:16 -0300

    upando novamente

--- Spring/blogpessoal/src/test/java/br/org/generation/blogpessoal/controller/TemaControllerTest.java
+++ Spring/blogpessoal/src/test/java/br/org/generation/blogpessoal/controller/TemaControllerTest.java
@@ -1,14 +1,48 @@
 package br.org.generation.blogpessoal.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import br.org.generation.blogpessoal.model.Tema;
+import br.org.generation.blogpessoal.model.Usuario;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TemaControllerTest {
 
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
+	@Test
+	@Order(1)
+	@DisplayName("Cadastrar Uma descricao")
+	public void deveCriarUmUsuario() {
+
+		HttpEntity<Tema> requisicao = new HttpEntity<Tema>(new Tema(0L, 
+			"Paulo Antunes"));
+
+		ResponseEntity<Usuario> resposta = testRestTemplate
+			.exchange("/tema/cadastrar", HttpMethod.POST, requisicao, Usuario.class);
+
+		assertEquals(HttpStatus.CREATED, resposta.getStatusCode());
+		assertEquals(requisicao.getBody().getDescricao(), resposta.getBody());
+	}
+
+	
+	
 }
